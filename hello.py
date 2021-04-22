5d52347fa292b8f77293655323205ec6bed783d9
William Santos <william.wapsantos@gmail.com>
2021-04-21 21:50:17 -0300

    First Commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+###########################################
+# William A. P. dos Santos                #
+# william.wapsantos@gmail.com             #
+# Estudos com Django                      #
+# 21 de abril de 2021                     #
+###########################################
+
+# Clear the terminal screen
+import os
+os.system('clear')
+
+'''
+	DATA TYPES
+	Strings
+	Numbers
+	Lists
+	Tuples
+	Dictionaries
+	Boolean
+'''
+
+
+texto = "Este diretorio foi criado para armazenar conteudo de estudo com Django!!!"
+print (texto)
