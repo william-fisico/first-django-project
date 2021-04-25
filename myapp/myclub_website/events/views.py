adb4831e31fb710d113b7ee7016cf42e51dd60b3
William Santos <william.wapsantos@gmail.com>
2021-04-25 13:24:03 -0300

    Criacao de usuarios

--- myapp/myclub_website/events/views.py
+++ myapp/myclub_website/events/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from .models import Event
 
-def home(request):
-    return render(request, 'home.html', {})
+def all_events(request):
+    event_list = Event.objects.all()
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "William"
+    month = month.capitalize()
+    # Convert month from name to number
+    month_number = int(list(calendar.month_name).index(month))
+
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # Get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # Get current time
+    #time = now.strftime('%I:%M:%S %p') # horas:minutos:segundos 12h
+    time = now.strftime('%H:%M') # horas:minutos 24h
+
+
+    return render(request, 'events/home.html', {
+        'name': name,
+        'year': year,
+        'month': month,
+        'month_number': month_number,
+        'cal' : cal,
+        'current_year': current_year,
+        'time': time,
+        })
